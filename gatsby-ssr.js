b2f364441092aa7e69b14db480571ec5415cd126
Amit Gawande <amit.w.gawande@gmail.com>
2022-11-02 22:06:19 +0530

    Add favicon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const headComponents = [
-    <link rel="stylesheet" href="css/custom.css" key="custom" />
+    <link rel="stylesheet" href="css/custom.css" key="custom" />,
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" key="favicon" />
   ]
   
   export const onRenderBody = ({ setHeadComponents }) => {
